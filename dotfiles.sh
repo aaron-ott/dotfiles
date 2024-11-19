f0a8bc66a739668255fbc8571b41e167c8d596d3
Aaron Ott <aaron.ott@merlinlabs.com>
2024-11-19 16:40:39 -0500

    Script finally works

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,48 +6,56 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-if [ $1 == "push" ]; then
-
-    SOURCE_PATH=""
-
-    if [ "$(uname)" == "Darwin" ]; then
-        SOURCE_PATH="~/.config"
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        SOURCE_PATH="~/.config"
-    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-        SOURCE_PATH="~/AppData/Local"
-    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-        SOURCE_PATH="~/AppData/Local"
-    fi
-
-    if [ -d $SOURCE_PATH/nvim ]; then
-        cp -r $SOURCE_PATH/nvim ./configs/
-    fi
-
-    if [ -d $SOURCE_PATH/alacritty ]; then
-        cp -r $SOURCE_PATH/alacritty ./configs/
-    fi
-
-elif [ $1 == "pull" ]; then
+if [ $1 == "pull" ]; then
 
     TARGET_PATH=""
 
     if [ "$(uname)" == "Darwin" ]; then
-        TARGET_PATH="~/.config/"
+        TARGET_PATH="$HOME/.config"
+        echo "Pulling for MacOS to path ${TARGET_PATH}"
     elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        TARGET_PATH="~/.config/"
+        TARGET_PATH="$HOME/.config"
+        echo "Pulling for Linux to path ${TARGET_PATH}"
     elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-        TARGET_PATH="~/AppData/Local/"
+        TARGET_PATH="$HOME/AppData/Local"
+        echo "Pulling for Windows to path ${TARGET_PATH}"
     elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-        TARGET_PATH="~/AppData/Local/"
+        TARGET_PATH="$HOME/AppData/Local"
+        echo "Pulling for Windows to path ${TARGET_PATH}"
     fi
 
-    if [ -d $TARGET_PATH ]; then
-        cp -r ./configs/nvim $TARGET_PATH
+    if [ -d "${TARGET_PATH}/nvim" ]; then
+        cp -r -v ./configs/nvim $TARGET_PATH/
     fi
 
-    if [ -d $TARGET_PATH ]; then
-        cp -r ./configs/alacritty $TARGET_PATH
+    if [ -d "${TARGET_PATH}/alacritty" ]; then
+        cp -r -v ./configs/alacritty $TARGET_PATH/
+    fi
+
+elif [ $1 == "push" ]; then
+
+    SOURCE_PATH=""
+
+    if [ "$(uname)" == "Darwin" ]; then
+        SOURCE_PATH="$HOME/.config"
+        echo "Pushing from MacOS path ${SOURCE_PATH}"
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        SOURCE_PATH="$HOME/.config"
+        echo "Pushing from Linux path ${SOURCE_PATH}"
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        SOURCE_PATH="$HOME/AppData/Local"
+        echo "Pushing from Windows path ${SOURCE_PATH}"
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+        SOURCE_PATH="$HOME/AppData/Local"
+        echo "Pushing from Windows path ${SOURCE_PATH}"
+    fi
+
+    if [ -d $SOURCE_PATH/nvim ]; then
+        cp -r -v $SOURCE_PATH/nvim ./configs/
+    fi
+
+    if [ -d $SOURCE_PATH/alacritty ]; then
+        cp -r -v $SOURCE_PATH/alacritty ./configs/
     fi
 
 else
