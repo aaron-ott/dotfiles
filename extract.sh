7541920fdb534d38d1c54eabd1ab627fd944f854
Aaron Ott <aaron.ott@merlinlabs.com>
2024-11-19 16:13:27 -0500

    Updated scripts to work on multiple platforms

--- extract.sh
+++ extract.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-if [ -d ~/.config/nvim ]; then
-	cp -r ~/.config/nvim ./configs/
+SOURCE_PATH=""
+
+if [ "$(uname)" == "Darwin" ]; then
+    SOURCE_PATH="~/.config"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    SOURCE_PATH="~/.config"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    SOURCE_PATH="~/AppData/Local"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    SOURCE_PATH="~/AppData/Local"
 fi
 
-if [ -d ~/.config/alacritty ]; then
-	cp -r ~/.config/alacritty ./configs/
+if [ -d $SOURCE_PATH/nvim ]; then
+    cp -r $SOURCE_PATH/nvim ./configs/
+fi
+
+if [ -d $SOURCE_PATH/alacritty ]; then
+    cp -r $SOURCE_PATH/alacritty ./configs/
 fi
